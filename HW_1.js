9e167ae3817228bfbe44f1b353a257e55aa107a0
Maksym Tsapiv <maksym.tsapiv@ucu.edu.ua>
2022-04-19 22:25:39 +0300

    Add methods

--- HW_1.js
+++ HW_1.js
@@ -1,2 +1,45 @@
+function reverseString1(str) {
+    return str.split("").reverse().join("");
+}
 
-console.log("hello")
+
+function reverseString2(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+
+function reverseString3(str) {
+    if (str === "")
+      return "";
+    else
+      return reverseString(str.substr(1)) + str.charAt(0);
+  }
+
+
+function reverseString4(str) {
+    var newString = "";
+    for (var i = 0; i < str.length; i++) {
+        newString = str[i] + newString;
+    }
+    return newString;
+}
+
+function reverseString5(str) {
+    let reversedStr = "";
+    for (let i in str) {
+        reversedStr = str[i] + reversedStr;
+    }
+    return reversedStr;
+}
+
+function reverseString6(str) {
+    let reversedStr = "";
+    for (let i of str) {
+        reversedStr = i + reversedStr;
+    }
+    return reversedStr;
+}
+
