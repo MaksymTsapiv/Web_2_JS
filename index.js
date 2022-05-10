543f4c81ce674bec3ef5625f1f1921effc90b55a
Maksym Tsapiv <maksym.tsapiv@ucu.edu.ua>
2022-05-10 23:51:41 +0300

    add some

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 /* YOUR CODE HERE! */
 
-var box = document.getElementById("box");
-var mouse = document.getElementById("mouse");
+let box = document.querySelector('.box');
+let number = 1;
 
-mouse.addEventListener("click", function(e) {
-    box.style.left = e.clientX + "px";
-    box.style.top = e.clientY + "px";
-});
+function changeColor() {
+    let colors = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'pink', 'brown', 'black'];
+    let random = Math.floor(Math.random() * colors.length);
+    box.style.backgroundColor = colors[random];
+}
+box.addEventListener('contextmenu', changeColor);
+
+function changeSize() {
+    box.classList.toggle('box-large');
+}
+
+box.addEventListener('click', (e) => {
+    if (e.shiftKey) changeSize();
+})
+
+function addBox(e) {
+    number += 1;
+    let newBox = e.target.cloneNode(true);
+    newBox.textContent = number;
+    console.log(e.target.offsetTop, e.target.offsetHeight);
+    newBox.style.top = (e.target.offsetTop + e.target.offsetHeight) + 'px';
+    newBox.style.left = (e.target.offsetLeft + e.target.offsetWidth) + 'px'
+    console.log(clonedBox.style.top);
+    box.appendChild(clonedBox);
+}
+function deleteBox(e) {
+    if (box.getElementsByTagName('*').length !== 0) {
+        box.removeChild(e.target);
+    }
+}
+
+box.addEventListener('dblclick', (e) => {
+    if (e.altKey) deleteBox(e);
+    else addBox(e); 
+})
